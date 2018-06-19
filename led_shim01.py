6f576c5042eb9219de4aedbe31e902f734538457
Blanchot <Blanchot@users.noreply.github.com>
2018-06-19 16:22:27 +0200

    added even more code

--- led_shim01.py
+++ led_shim01.py
@@ -5,8 +5,8 @@ import time
 
 ledshim.set_clear_on_exit()
 
-rise = (0,255,0)
-fall = (255,0,0)
+rise = (0,32,0) #too bright
+fall = (128,0,0) #maybe too bright
 same = (0,0,255)
 
 print('Try: fallWalk(), riseWalk(), sameWalk()')
@@ -38,6 +38,37 @@ def sameWalk():
     ledshim.show()
     time.sleep(0.2)
   reset()
+
+def test():
+  ledshim.clear()
+  ledshim.set_pixel(0,*rise)
+  ledshim.set_pixel(1,*rise)
+  ledshim.set_pixel(2,*same)
+  ledshim.set_pixel(3,*rise)
+  ledshim.set_pixel(4,*rise)
+  ledshim.set_pixel(5,*rise)
+  ledshim.set_pixel(6,*same)
+  ledshim.set_pixel(7,*same)
+  ledshim.set_pixel(8,*fall)
+  ledshim.set_pixel(9,*fall)
+  ledshim.set_pixel(10,*fall) 
+  ledshim.set_pixel(11,*rise)
+  ledshim.set_pixel(12,*rise)
+  ledshim.set_pixel(13,*same)
+  ledshim.set_pixel(14,*rise)
+  ledshim.set_pixel(15,*rise)
+  ledshim.set_pixel(16,*rise)
+  ledshim.set_pixel(17,*same)
+  ledshim.set_pixel(18,*same)
+  ledshim.set_pixel(19,*fall)
+  ledshim.set_pixel(20,*fall)
+  ledshim.set_pixel(21,*fall)
+  ledshim.set_pixel(22,*rise)
+  ledshim.set_pixel(23,*rise)
+  ledshim.set_pixel(24,*same)
+  ledshim.set_pixel(25,*rise)
+  ledshim.set_pixel(26,*rise)
+  ledshim.set_pixel(27,*rise)
 
 
 '''
