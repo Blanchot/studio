432345d3a84c07f1ba3138e6909b1e8c4d1570a5
Blanchot <3512173+Blanchot@users.noreply.github.com>
2018-06-19 15:47:45 +0200

    Added some code

--- led_shim01.py
+++ led_shim01.py
@@ -1,8 +1,30 @@
 # Code test for Pimoroni's LED Shim
 
 import ledshim
+import time
 
 ledshim.set_clear_on_exit()
+
+'''
+CONSTANTS
+ledshim.NUM_PIXELS
+ledshim.DISPLAY_WIDTH # Same as NUM_PIXELS?
+
+FUNCTIONS
+ledshim.clear()
 ledshim.set_brightness(0.6)
 ledshim.set_pixel(0,0,0,0)
-ledshim.NUM_PIXELS
+ledshim.show()
+'''
+
+'''
+#Pimoroni test.py code
+
+for col in ((255, 0, 0), (0, 255, 0), (0, 0, 255)):
+    r, g, b = col
+    for x in range(ledshim.DISPLAY_WIDTH):
+        ledshim.clear()
+        ledshim.set_pixel(x, r, g, b)
+        ledshim.show()
+        time.sleep(0.2)
+'''
