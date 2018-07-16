d3af88851beb34b881ee84b14b7aaa7905607be3
Blanchot <Blanchot@users.noreply.github.com>
2018-07-16 11:31:27 +0200

    Added iota balance check

--- flask_tests/flask_with_template.py
+++ flask_tests/flask_with_template.py
@@ -3,24 +3,52 @@
 # Flask uses a template engine called Jinja2 so that you can use separate HTML files
 # with placeholders for spots where you want dynamic data to be inserted.
 
-# import the main module and the render_template
+# Import the flask main module and the render_template
 from flask import Flask, render_template
 import datetime
+
+# Import the PyOTA library
+from iota import Iota
+from iota import Address
+
 app = Flask(__name__)
+# URL to IOTA fullnode used when checking balance
+iotaNode = "https://field.carriota.com:443"
+# Create an IOTA object
+api = Iota(iotaNode, "")
+# Thalia Receive address:
+address = [Address(b'RNSVVCTUYTCMZVTUAOUZUZSXKE9XZGUNAG9XNDLEKXFUDE9MSLAEQIJRFIFUCRFIZFCZNZAYFDJFQFELZMFOWWJNTD')]
+
+# Function for checking address balance on the IOTA tangle. 
+def checkbalance():
+  #print("Checking balance")
+  gb_result = api.get_balances(address)
+  balance = gb_result['balances']
+  #print('Balance is: ',balance[0])
+  bal_text = 'Balance: ' + str(balance[0])
+  return bal_text
+
+
 
 @app.route("/")
 def hello():
+  iota_balance = checkbalance()
   now = datetime.datetime.now()
   timeString = now.strftime("%Y-%m-%d %H:%M")
+  
   # Create a dictionary of variables (a set of keys, such as title that are associated with values, 
   # such as HELLO!) to pass into the template
   templateData = {
-    'title' : 'HELLO!',
+    'title' : 'THALIA',
     'time': timeString
+    'balance': iota_balance
     }
   # Return the main.html template to the web browser using the variables in the templateData dictionary
   return render_template('main.html', **templateData)
 
+
+
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80, debug=True)
+
 
