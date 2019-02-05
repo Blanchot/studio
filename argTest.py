6ee8b9a3ca131892adca6835447225735e5bd848
Blanchot <Blanchot@users.noreply.github.com>
2019-02-05 08:31:23 +0100

    first test

--- argTest.py
+++ argTest.py
@@ -1,1 +1,19 @@
 # Argument parsing test
+
+import argparse
+
+parser= argparse.ArgumentParser()
+parser.add_argument("secs", help="convert seconds into more human readable form", type=int)
+args= parser.parse_args()
+
+days= args.secs//86400
+rem_secs= args.secs%86400
+
+hrs= rem_secs//3600
+rem_secs = rem_secs%3600
+
+mins = rem_secs//60
+
+print('Next stake:',days,'days,',hrs,'hours,',mins,'minutes')
+  
+
